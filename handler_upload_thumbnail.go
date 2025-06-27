71dfc68fa1a5e693d771e1c6c17e7c105999f879
gainax2k1 <gainax2k1@gmail.com>
2025-06-26 23:02:39 -0700

    CH3:L7

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+const maxMemory = 10 << 20 // Bit shift 10 to the left 20 times to get 10Mb
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +37,76 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse form", err)
+		return
+	}
+
+	fileData, fileHeaders, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't retrieve form file data and/or header", err)
+		return
+	}
+
+	mediaHeader := fileHeaders.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(mediaHeader)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to parse mime", err)
+		return
+	}
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "filetype not jpeg or png", err)
+		return
+	}
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Authenticated user is not the video owner", err)
+		return
+	}
+
+	// str := base64.StdEncoding.EncodeToString(imageData)
+
+	//root := cfg.assetsRoot // temp for figuring out
+	mediaSuffix := (strings.Split(mediaType, "/"))[1]
+
+	key := make([]byte, 32)
+	rand.Read(key)
+	keyString := base64.RawURLEncoding.EncodeToString(key)
+	// ****** change videoidstring here to new generated string ***********
+	joinedPath := filepath.Join(cfg.assetsRoot, (keyString + "." + mediaSuffix))
+	newFile, err := os.Create(joinedPath)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to create file", err)
+		return
+	}
+	defer newFile.Close() // alwasy defer a close on create
+
+	_, err = io.Copy(newFile, fileData)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to copy file", err)
+		return
+	}
+
+	thumbnailURL := "http://localhost:8091/" + joinedPath //"data:" + mediaType + ";base64," + str
+	videoMetadata.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(videoMetadata)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
